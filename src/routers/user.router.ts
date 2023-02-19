5c7e6f0b391c3c2ec883508d89f2e36e8c329661
Slavy Spasoff <slavyspasoff@gmail.com>
2023-02-19 12:15:04 +0100

    add add/remove friend user controller

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -5,6 +5,7 @@ import {
   getAllUsers,
   getUser,
   updateUser,
+  addRemoveFriend,
 } from '../controllers/user.controller.js'
 import { profileUploads } from '../middleware/multerUploads.middleware.js'
 
@@ -14,5 +15,5 @@ router.use(passport.authenticate('jwt', { session: false }))
 
 router.get('/', getAllUsers)
 router.route('/:id').get(getUser).patch(profileUploads, updateUser)
-
+router.patch('/addRemoveFriends/:id', addRemoveFriend)
 export { router as default }
